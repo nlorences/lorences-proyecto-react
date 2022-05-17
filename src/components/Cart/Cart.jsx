46c798ea16f1605144e283f07a92ab07ab0f50d5
nicolas.lorences <nico.lorences@gmail.com>
2022-05-17 01:26:52 -0300

    update cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React, {useContext} from 'react'
+import {Link} from 'react-router-dom';
 import CartContext from '../../context/CartContext'
 import CartItem from '../CartItem/CartItem'
+import Button from './../Button/Button'
 import './Cart.css'
 
 const Cart = () => {
@@ -8,10 +10,22 @@ const Cart = () => {
 
 
   return (
-    <div className="cart-container">
-      <h2 className="cart-title">Carrito</h2>
-      {cartCtx.products.map((item) => (<CartItem key={item.id} item={item}/>))}    
-    </div>
+    cartCtx.totalCount() > 0 ?
+      <div className="cart-container">
+        <h2 className="cart-title">Carrito</h2>
+        <div className="cart-item-container">
+          {cartCtx.products.map((item) => (<CartItem key={item.id} item={item}/>))}    
+          <div className="cart-checkout">        
+          <p>Total de la compra $ {cartCtx.totalAmount()} </p>
+          <Button btnText="Finalizar compra"></Button>
+          </div>
+        </div>
+      </div>
+    : 
+      <div className="cart-empty">
+        <h2 className="cart-title">Tu carrito está vacio</h2>
+        <Link to="/"> <Button btnText="Volver" btnClass="btn-small"></Button> </Link>
+      </div>
   )
 }
 
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { Link } from 'react-router-dom';
+import CartContext from '../../context/CartContext'
 import './CartItem.css'
+import Button from '../Button/Button';
 
 const CartItem = ({item}) => {
+  const cartCtx = useContext(CartContext);
+  
   return (
-    <div className="cart-item-container">
-        <p>{item.title}</p>
-        <p className="cart-item-quantity">{item.quantity}</p>
+    <div className="cart-item">
+      <div className="cart-item-left">
+        <Link to={'/item/' + item?.id}> <img className="cart-item-img" src={item?.pictureUrl} alt="imagen del producto" /> </Link>
+        <Link to={'/item/' + item?.id} className="product-link"> <p>{item?.title}</p> </Link>
+      </div>
+      <div className="cart-item-rigth">
+        <div className="cart-item-info">
+          <p>Precio unitario</p>
+          <p>$ {item?.price}</p>
+        </div>
+        <div className="cart-item-info">
+          <p>Unidades</p>
+          <p>{item?.quantity}</p>
+        </div>
+        <div className="cart-item-info">
+          <p>Total</p>
+          <p>$ {item?.price * item?.quantity}</p>
+        </div>
+        <Button btnText="Quitar" btnClass="btn-xs" action={() => cartCtx.deleteById(item.id)}></Button>
+      </div>
     </div>
   )
 }
